a5bd9f3c8b9d765114f108266d382ff67ecdd78e
Idris Bahce <idrisbahce26@gmail.com>
2025-10-03 23:31:09 +0200

    updated

---
+++ SevenSeg.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+SDI = 17
+RCLK = 18
+SRCLK = 27
+
+segCode = [0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71]
+
+def set_up():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SDI, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(RCLK, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(SRCLK, GPIO.OUT, initial=GPIO.LOW)
+
+
+def hc595_shift(dat):
+    for bit in range(0, 8):
+        GPIO.output(SDI, 0x80 & (dat << bit))
+        GPIO.output(SRCLK, GPIO.HIGH)
+        time.sleep(0.001)
+        GPIO.output(SRCLK, GPIO.LOW)
+    
+    GPIO.output(RCLK, GPIO.HIGH)
+    time.sleep(0.001)
+    GPIO.output(RCLK, GPIO.LOW)
+
+def main():
+    while True:
+        for code in segCode:
+            hc595_shift(code)
+            print ("segCode[%s]: 0x%02X"%(segCode.index(code), code))
+            time.sleep(0.5)
+
+def destroy():
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    set_up()
+    try:
+        main()
+    except KeyboardInterrupt:
+        destroy()
